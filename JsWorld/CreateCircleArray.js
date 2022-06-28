e86aaba4fc3cb93a48de4227251f081c43d12c06
NgocThach <19520265@gm.uit.edu.vn>
2022-06-28 20:34:32 +0700

    Xong bình trà

--- JsWorld/CreateCircleArray.js
+++ JsWorld/CreateCircleArray.js
@@ -1,14 +1,22 @@
-function createCircleArray(cx=0,cy=0,cz=0, r = 10, step = 100) {
+function createCircleArray(cx1=0,cy1=0,cz1=0, r1 = 10, step = 100, cx2 = 0, cy2 = 0, cz2 = 0, r2 = 10, step2 = 100) {
     const vertices = [];
 
-    for (var i = 0; i <= step; i++){
-        var nx = r*Math.cos(i*2*Math.PI/step);
-        var ny = r*Math.sin(i*2*Math.PI/step);
-        var ox = r*Math.cos((i-1)*2*Math.PI/step);
-        var oy = r*Math.sin((i-1)*2*Math.PI/step);
-        var oz = 0;
-        vertices.push(cx,cy,cz, cx + nx,cy +0, cz+ ny, cx + ox, cy + oz, cz+ oy);
+    for (var i = 1; i <= step; i++){
+        var nx1 = r1*Math.cos(i*2*Math.PI/step);
+        var ny1 = r1*Math.sin(i*2*Math.PI/step);
+        var ox1 = r1*Math.cos((i-1)*2*Math.PI/step);
+        var oy1 = r1*Math.sin((i-1)*2*Math.PI/step);
+
+        var nx2 = r2*Math.cos(i*2*Math.PI/step);
+        var ny2 = r2*Math.sin(i*2*Math.PI/step);
+        var ox2 = r2*Math.cos((i-1)*2*Math.PI/step);
+        var oy2 = r2*Math.sin((i-1)*2*Math.PI/step);
+        vertices.push(cx2+nx2,cy2+0,cz2+ny2,  cx1+nx1,cy1+0,cz1+ny1,  cx1+ox1,cy1,cz1+oy1);
+        vertices.push(cx2+nx2,cy2+0,cz2+ny2,  cx1+ox1,cy1+0,cz1+oy1,  cx2+ox2,cy2,cz2+oy2);
+        // vertices.push(cx1 + nx1, cy1 + ny1, 0,   cx1 + ox1, cy1 + oy1, 0,     cx2 + ox2, cy2 + oy2, 0);
+        // vertices.push(cx1 + nx1, cy1 + ny1, 0,   cx2 + nx2, cy2 + ny2, 0,     cx2 + ox2, cy2 + oy2, 0);
     }
+
     return vertices;
 }
 
--- JsWorld/Teapot.js
+++ JsWorld/Teapot.js
@@ -15,28 +15,41 @@ function createTeapot(scale = 1){
     const head = createSphere(1.8, 30, 30, 10, 0, false, 0, 0, 0.5, false, 0xd4d4d4);
     const tophead = createSphere(0.5, 30, 30, 15, 0, false, 0, 0, 2.3, false, 0xe4e4e4);
     const gun = createCylinder(1, 0,3.8,0, 0.5, 1, 50, false);
-    const tail = createSphere(1, 30, 30, 0, 0, false, 0, -5, 0, true, 0xc4c4c4);
-    const gunn = createGun(0, 3, 0, 0, 0);
+    var tailX = [1, 2, 4, 3];
+    var tailY = [-3, 0, 2, -4];
+    var gunnX = [0, 1, 3, 3];
+    var gunnY = [0, 0, 0, 2.5];
+    const tail = createGun(0, 0.3, 0, 0, 0, true, 100, tailX, tailY, false);
+    const gunn = createGun(0, 2.5, 0, 0, 0, true, 100, gunnX, gunnY, true);
+    gunn.rotation.x = 1;
+    gunn.rotation.y = 1.55;
+    gunn.rotation.z += 6.6;
+    gunn.position.y += 1;
+    tail.position.y -= 5;
+    tail.rotation.x += 1.55;
+    tail.rotation.y += 1.55;
+    tail.rotation.z += 1.55;
+    tail.position.z -= 1.55;
+
     tail.scale.set(0.75, 0.75, 0.75);
 
     const mesh = new THREE.Mesh(teapot, material);
     mesh.add(body);
     mesh.add(head);
     mesh.add(tophead);
-    mesh.add(gun);
+    // mesh.add(gun);
     mesh.add(tail);
     mesh.add(gunn);
     mesh.rotation.set(Math.PI/2, Math.PI, Math.PI);
-
+    mesh.scale.set(0.7, 0.8, 1);
     mesh.tick = () => 
         Rotate(mesh);
     return mesh;
 }
 
-function createGun(r1 = 0, r2 = 3, cx = 0, cy = 0, cz = 0, wireframe = false, step = 100){
-    var bezierX = [0, 0, 15, 15];
-    var bezierY = [0, -20, 0, 20];
+function createGun(r1 = 0, r2 = 3, cx = 0, cy = 0, cz = 0, wireframe = true, step = 100, bezierX = [], bezierY = [], smaller = true){
     var xu = 0.0 , yu = 0.0 , u = 0.0 ;
+    var pxu = 0.0, pxy = 0.0;
     var u = 0 ;
     const Gun = new THREE.BufferGeometry();
     const material = new THREE.MeshBasicMaterial({
@@ -45,10 +58,19 @@ function createGun(r1 = 0, r2 = 3, cx = 0, cy = 0, cz = 0, wireframe = false, st
         wireframe: wireframe,
     });
     var vertices = [];
-    for(u = 0.0 ; u <= 1.0 ; u += 1/step) {
-        xu = Math.pow(1-u,3)*bezierX[0]+3*u*Math.pow(1-u,2)*bezierX[1]+3*Math.pow(u,2)*(1-u)*bezierX[2] + Math.pow(u,3)*bezierX[3];
-        yu = Math.pow(1-u,3)*bezierY[0]+3*u*Math.pow(1-u,2)*bezierY[1]+3*Math.pow(u,2)*(1-u)*bezierY[2] + Math.pow(u,3)*bezierY[3];
-        vertices.push.apply(vertices, createCircleArray(xu, yu, 0, 5, 100));
+    for(u = 1/step ; u <= 1.0 ; u += 1/step) {
+        var ou = u - 1/step;
+        pxu = Math.pow(1-ou,3)*bezierX[0]+3*ou*Math.pow(1-ou,2)*bezierX[1]+3*Math.pow(ou,2)*(1-ou)*bezierX[2] + Math.pow(ou,3)*bezierX[3];
+        pxy = Math.pow(1-ou,3)*bezierY[0]+3*ou*Math.pow(1-ou,2)*bezierY[1]+3*Math.pow(ou,2)*(1-ou)*bezierY[2] + Math.pow(ou,3)*bezierY[3];
+        xu =  Math.pow(1-u,3)*bezierX[0]+3*u*Math.pow(1-u,2)*bezierX[1]+3*Math.pow(u,2)*(1-u)*bezierX[2] + Math.pow(u,3)*bezierX[3];
+        yu =  Math.pow(1-u,3)*bezierY[0]+3*u*Math.pow(1-u,2)*bezierY[1]+3*Math.pow(u,2)*(1-u)*bezierY[2] + Math.pow(u,3)*bezierY[3];
+        var R1 = r2;
+        if(smaller == true)
+            R1 = Math.max(r2 / step * (100 - u * 100), r2/(step / 10));
+        var R2 = r2;
+        if(smaller == true)
+            R2 = Math.max(r2 / step * (100 - ou * 100), r2/(step / 10));
+        vertices.push.apply(vertices, createCircleArray(xu, yu, 0, R1, 100, pxu, pxy, 0, R2, 100));
     }
     Gun.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
     const mesh = new THREE.Mesh(Gun, material);
